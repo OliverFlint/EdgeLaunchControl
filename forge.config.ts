203718fd4046bc2a47cd32cd54e91f395f93af1f
Oliver Flint <oliverflint@gmail.com>
2026-03-04 10:33:08 +0000

    fixed icon issue

--- forge.config.ts
+++ forge.config.ts
@@ -1,13 +1,11 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { PublisherGithub } from '@electron-forge/publisher-github';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import path from 'path';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -15,14 +13,14 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: './src/img/icon.ico',
+    icon: './src/img/elc.ico',
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({
       name: 'edgelaunchcontrol',
       setupExe: 'edgelaunchcontrol-setup.exe',
-      setupIcon: './src/img/logo.ico',
+      setupIcon: './src/img/elc.ico',
     }),
   ],
   publishers: [
